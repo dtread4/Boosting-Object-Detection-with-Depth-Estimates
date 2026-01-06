d8b2b76a19c68491af02cf0e1ac21f6883566f5f
David Treadwell <dtread4@live.com>
2026-01-06 11:53:06 -0800

    Update Depth Anything 3 test to better reflect how it will be used in training/inference

--- setup_tests/test_da3.py
+++ setup_tests/test_da3.py
@@ -1,12 +1,15 @@
 # David Treadwell
 #
-# Use this script to check that your DepthAnywhere3 installation was correct
+# Use this script to check that your DepthAnywhere3 installation was correct.
+# It also shows how the process will calculate depth maps (with resizing) in the main training/inference scripts
+#
 # Adapted from the official DA3's HuggingFace page: https://huggingface.co/depth-anything/DA3-BASE
 # and the da3 notebook on the official repo: https://github.com/ByteDance-Seed/Depth-Anything-3/blob/main/notebooks/da3.ipynb
 
 import torch
 import argparse
 
+from PIL import Image
 import cv2
 import numpy as np
 
@@ -15,33 +18,18 @@ from depth_anything_3.utils.visualize import visualize_depth
 
 import matplotlib.pyplot as plt
 
-def _normalize_depth_mask(depth_map):
-        """_summary_
-
-        Args:
-            depth_map: _description_
-
-        Returns:
-            _description_
-        """
-        min_val = np.min(depth_map)
-        max_val = np.max(depth_map)
-
-        # If no unique depths are computed, return an array of 0's
-        if min_val == max_val:
-            print("min equaled max)")
-            return np.zeros(depth_map.shape).astype(np.float32)
-        
-        # Convert to float and normalize
-        normalized_image = (depth_map - min_val) / (max_val - min_val)
-        return normalized_image
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image_path', required=True, type=str,
-                        help="The image path to get the depth map for")
+    parser.add_argument('--image_paths', required=True, type=str, nargs='+',
+                        help="The image paths to get the depth map for")
     args = parser.parse_args()
+
+    image_paths = args.image_paths
+    images = [Image.open(img_pth).convert("RGB") for img_pth in image_paths]
+    min_size = min(min(img.size) for img in images)
+    print("Min image dimension:", min_size)
+    images_resized = [img.resize((min_size, min_size), Image.Resampling.BILINEAR) for img in images]
 
     # Load model from Hugging Face Hub
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -49,35 +37,27 @@ if __name__ == "__main__":
     model = model.to(device=device)
 
     # Run inference on images (this is done here how it will be in the main program)
-    img = cv2.imread(args.image_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    images = [img]  # List of image paths, PIL Images, or numpy arrays
     prediction = model.inference(
-        images,
-        process_res=max(img.shape[0], img.shape[1]),
-        process_res_method="upper_bound_resize"
+        images_resized,
+        process_res=min_size
     )
-    depth_map = cv2.resize(prediction.depth[0], (img.shape[1], img.shape[0]), interpolation=cv2.INTER_LINEAR)
-    depth_mask_n = _normalize_depth_mask(depth_map)
-
-    # Access results
-    print("Image shape:", img.shape)
-    print("Depth map shape:", depth_map.shape)        # Depth maps: [N (image index), H, W] float32
-    print(f"Min value in image: {np.min(img)} | Max value in image: {np.max(img)}")
-    print(f"Min value in depth map: {np.min(depth_map)} | Max value in depth map: {np.max(depth_map)}")
 
     # Display depth map
-    fig, axes = plt.subplots(2, 1)
+    fig, axes = plt.subplots(2, len(images))
+
+    if len(images) == 1:
+        axes = axes.reshape(2, 1)
 
     # Display original image to left of depth map
-    axes[0].imshow(img)
-    axes[0].set_title("Original image")
-    axes[0].axis('off')
+    for i in range(len(images)):
+        axes[0, i].imshow(images[i])
+        axes[0, i].set_title("Original image")
+        axes[0, i].axis('off')
 
-    # Display the depth map
-    axes[1].imshow(visualize_depth(depth_map, cmap='Spectral'))
-    axes[1].set_title("Estimated depth map")
-    axes[1].axis('off')
+        # Display the depth map
+        axes[1, i].imshow(visualize_depth(cv2.resize(prediction.depth[i], images[i].size, cv2.INTER_LINEAR), cmap="Spectral"))
+        axes[1, i].set_title("Estimated depth map")
+        axes[1, i].axis('off')
 
     plt.show()
 
