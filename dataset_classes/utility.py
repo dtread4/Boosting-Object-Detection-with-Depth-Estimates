5cf21e3d92c03965afc187f895a2444ccca13b3d
David Treadwell <dtread4@live.com>
2026-01-08 09:59:20 -0800

    Update transforms for proper image preprocessing (before depth added)

--- dataset_classes/utility.py
+++ dataset_classes/utility.py
@@ -4,8 +4,7 @@ import torchvision.transforms as T
 
 def get_transforms(train=True):
     """
-    Returns the necessary transformations to apply to images
-    Assumes the image is already in tensor format, with the dimensions (batch, channels, height, width)
+    Returns the necessary transformations to apply to images when initially loaded
 
     Args:
         train: Flag to only applies augmentation-based transformations to images in the training set. Defaults to True.
@@ -13,7 +12,7 @@ def get_transforms(train=True):
     Returns:
         The set of transformations to use, ready for use by the dataset
     """
-    transforms = []
+    transforms = [T.ToTensor()]
     if train:
         transforms.append(T.RandomHorizontalFlip(0.5))
     return T.Compose(transforms)
